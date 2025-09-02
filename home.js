f5d8aa2e9e72be74c82d445cc7aa3954e13b87f2
Panchal Dhairya <panchaldhairya1620@gmail.com>
2025-09-02 22:50:46 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,24 +1,11 @@
-let qrImage = document.getElementById("qrImage"); 
-let downloadBtn = document.getElementById("downloadBtn");
+const wrapper = document.querySelector(".qr_container");
+const generateBtn = document.querySelector("form button");
+const qrInput = document.querySelector("form input");
+const qrImg = document.querySelector(".qr_container img");
 
-function generateQR() {
-    let qrText = document.getElementById("qrText").value;
-    if (qrText.trim().length === 0) {
-        alert("Please enter a URL or text!");
-        return;
-    }
-
-    // Generate QR Code from API
-    qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=" + qrText;
-
-    // Show download button
-    downloadBtn.style.display = "block";
-}
-
-// Download QR Code
-downloadBtn.addEventListener("click", () => {
-    let link = document.createElement("a");
-    link.href = qrImage.src;   // QR image source
-    link.download = "qrcode.png"; // File name
-    link.click();
+generateBtn.addEventListener("click", () => {
+  let qrValue = qrInput.value.trim();
+  if (!qrValue) return;
+  qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`;
+  wrapper.classList.add("show");
 });
