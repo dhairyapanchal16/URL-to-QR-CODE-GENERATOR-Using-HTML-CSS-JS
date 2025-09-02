e82c0c34beb9b7f8aaa73b93cb5563415fa4e04f
Panchal Dhairya <panchaldhairya1620@gmail.com>
2025-09-02 22:52:30 +0530

    Update home.js

--- home.js
+++ home.js
@@ -3,9 +3,19 @@ const generateBtn = document.querySelector("form button");
 const qrInput = document.querySelector("form input");
 const qrImg = document.querySelector(".qr_container img");
 
-generateBtn.addEventListener("click", () => {
+generateBtn.addEventListener("click", (e) => {
+  e.preventDefault(); // prevent form refresh
   let qrValue = qrInput.value.trim();
-  if (!qrValue) return;
-  qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`;
+  if (!qrValue) {
+    alert("Please enter a URL or text!");
+    return;
+  }
+
+  // Generate QR using API
+  qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(
+    qrValue
+  )}`;
+
+  // Show QR container
   wrapper.classList.add("show");
 });
